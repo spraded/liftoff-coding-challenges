67d5fa41ba46dbe90113f5de5fd776a413413501
Emily Spradling <spradling.ed@gmail.com>
2023-02-17 08:27:55 -0600

    week 7 coding challenge completed

---
+++ week-7-age-compare.js
@@ -0,0 +1,17 @@
+class Person {
+	constructor(name, age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	compareAge(other) {
+		// Write code here!
+		if (this.age > other.age){
+			return other.name + " is younger than me."
+		} else if (this.age < other.age){
+			return other.name + " is older than me."
+		} else {
+			return other.name + " is the same age as me."
+		}
+	}
+}
